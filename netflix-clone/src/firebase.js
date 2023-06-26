fd81736b2d229cdeb4683485b294508a0a61b739
Atul Goyal <atulgoyal0607@gmail.com>
2023-06-26 10:53:17 +0530

    1st commit

---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAWqXh9vS40N1WWnNe8xhjI9eOnUkwZbr0",
+  authDomain: "netflix-clone-26cfd.firebaseapp.com",
+  projectId: "netflix-clone-26cfd",
+  storageBucket: "netflix-clone-26cfd.appspot.com",
+  messagingSenderId: "437791966409",
+  appId: "1:437791966409:web:77b7815f1b24f0647ab918"
+};
